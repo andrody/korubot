e4bd399643370e8f65265685d86daecac29d0d8b
Andrew Feitosa <andrewcsz@gmail.com>
2018-11-21 17:48:22 -0300

    to production

--- contants.js
+++ contants.js
@@ -1,12 +1,12 @@
 const USERS = [
     { discordUser: "130404454401835008", name: "Andrody" },
-    // { discordUser: "132292098752905216", name: "Bruno" },
-    // { discordUser: "133715213240369152", name: "Mero" },
-    // { discordUser: "132295133914726401", name: "Isaac" },
+    { discordUser: "132292098752905216", name: "Bruno" },
+    { discordUser: "133715213240369152", name: "Mero" },
+    { discordUser: "132295133914726401", name: "Isaac" },
 ]
 
-// const CHANNEL_GENERAL = "130406987388289025"
-const CHANNEL_GENERAL = "314465869520568320"
+const CHANNEL_GENERAL = "130406987388289025" // GENERAL KORUJA
+// const CHANNEL_GENERAL = "314465869520568320" TESTES HADO
 
 module.exports = {
     USERS,
